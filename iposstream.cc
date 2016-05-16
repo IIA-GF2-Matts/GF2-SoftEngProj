2977a7a1de5f216b35632195babc9e5e989e7fa6
Matt Diesel <md639@cam.ac.uk>
2016-05-16 21:16:20 +0000

    Added positional character stream for use with scanner

---
+++ iposstream.cc
@@ -0,0 +1,100 @@
+
+#include "iposstream.h"
+
+
+// struct SourcePos
+
+SourcePos::SourcePos()
+	: SourcePos( 0, 0, 0 ) {
+}
+
+SourcePos::SourcePos( int line, int col, int abs )
+	: SourcePos( line, col, abs, abs - col ) {
+}
+
+SourcePos::SourcePos( int line, int col, int abs, int lineStart )
+	: Line( line ),
+	  Column( col ),
+	  Abs( abs ),
+	  LineStart( lineStart ) {
+}
+
+std::ostream& operator<<( std::ostream& os, const SourcePos& sp ) {
+	return( os << sp.Line << ":" << sp.Column );
+}
+
+
+// class iposstream
+
+// Creates an uninitialised positional stream
+iposstream::iposstream()
+	: _open(false), Pos( 0, 0, 0 ), TabWidth( 1 ), next( 1, 1, 1 ) {
+}
+
+
+// Creates a positional stream based on an input stream.
+iposstream::iposstream( std::istream* base )
+	: _open(true), Pos( 0, 0, 0 ), TabWidth( 1 ), basestream( base ), next( 1, 1, 1 ) {
+}
+
+iposstream::~iposstream() {
+}
+
+// reads the next character without extracting it
+int iposstream::peek() {
+	return( basestream->peek() );
+}
+
+// Extracts the next character from the stream, and increments the counters.
+// Regardless of operating system, this function will return '\n' for a line
+// ending.
+int iposstream::get() {
+	this->Pos = next;
+
+	if (basestream->eof()) {
+		return '\0';
+	}
+
+	int ret = basestream->get();
+	this->next.Abs += 1;
+
+	// Handle \r\n sequence, required when using windows line endings on a non
+	// windows operating system.
+	if ( ret == '\r' ) {
+		if ( this->peek() == '\n' ) {
+			basestream->get();
+			this->next.Abs += 1;
+		}
+
+		ret = '\n';
+	}
+
+	// Eol characters are reported at the end of the line, rather than at the
+	// beginning of the next
+	if ( ret == '\n' ) {
+		this->next.Line += 1;
+		this->next.Column = 1;
+		this->next.LineStart = this->next.Abs + 1;
+	}
+	// else if ( ret == '\t' ) { // Tab is not always counted as 1 character.
+	// 	this->next.Abs += this->TabWidth - 1;
+	// 	this->next.Column += this->TabWidth;
+	// }
+	else {
+		this->next.Column += 1;
+	}
+
+	return( ret );
+}
+
+// checks if end-of-file has been reached
+bool iposstream::eof() const {
+	return( basestream->eof() );
+}
+
+
+// Set the base stream
+void iposstream::setStream(std::istream* is) {
+	basestream = is;
+	_open = true;
+}
---
+++ iposstream.h
@@ -0,0 +1,50 @@
+
+#include <string>
+#include <istream>
+
+
+#ifndef UTIL_IPOSSTREAM_H
+#define UTIL_IPOSSTREAM_H
+
+
+// Represents a position within a file.
+struct SourcePos {
+	// std::string& File;
+	int Line;
+	int Column;
+	int Abs;
+	int LineStart;
+
+	SourcePos();
+	SourcePos( int, int, int );
+	SourcePos( int, int, int, int );
+
+	friend std::ostream& operator<<( std::ostream&, const SourcePos& );
+};
+
+
+// An input stream that also records character positions.
+// Todo: Make this into a fully featured stream class derived from std::istream
+class iposstream {
+public:
+	iposstream();
+	iposstream( std::istream* );
+	~iposstream();
+
+	int peek();
+	int get();
+	bool eof() const;
+
+	void setStream(std::istream* );
+
+	SourcePos Pos;
+	int TabWidth;
+
+private:
+	bool _open;
+	std::istream* basestream;
+	SourcePos next;
+};
+
+
+#endif
