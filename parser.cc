78921700ffd2d4e057caedb7229b95b827c7c091
Matt Diesel <md639@cam.ac.uk>
2016-05-17 23:40:10 +0000

    Updated empty parser.cc to match header changes

--- parser.cc
+++ parser.cc
@@ -9,17 +9,9 @@ using namespace std;
 bool parser::readin (void)
 {
   /* over to you */
+    return false;
 }
 
-parser::parser (network* network_mod, devices* devices_mod,
-		monitor* monitor_mod, scanner* scanner_mod)
-{
-  netz = network_mod;  /* make internal copies of these class pointers */
-  dmz = devices_mod;   /* so we can call functions from these classes  */
-  mmz = monitor_mod;   /* eg. to call makeconnection from the network  */
-  smz = scanner_mod;   /* class you say:                               */
-                       /* netz->makeconnection (i1, i2, o1, o2, ok);   */
-
-  /* any other initialisation you want to do? */
-
+parser::parser(network* netz, devices* devz, monitor* mons, scanner& scan, names* nms)
+    : _netz(netz), _devz(devz), _mons(mons), _scan(scan), _nms(nms), errs(cout) {
 }
