438b8a820a712b73f51fbf0c42521dd41cd90e53
Matt Judge <mcj33@cam.ac.uk>
2016-05-21 04:21:51 +0100

    Added device names and dtype output pins sets to autocorrect.h (may require moving)

--- autocorrect.h
+++ autocorrect.h
@@ -1,6 +1,26 @@
 #include <set>
 #include "cistring.h"
 
+
+const std::set<cistring> devicesset = {
+  "CLOCK",
+  "SWITCH",
+  "AND",
+  "NAND",
+  "OR",
+  "NOR",
+  "XOR",
+  "DTYPE"
+};
+
+const std::set<cistring> dtypeoutset = {
+  "DATA", 
+  "CLK", 
+  "SET",
+  "CLEAR"
+};
+
+
 // takes a cistring input and returns a set containing the
 // closest matches from a set of candidates, along with the 
 // calculated distance between them. Lower means a closer match.
