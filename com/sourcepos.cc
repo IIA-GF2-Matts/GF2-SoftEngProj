c820d184fc66039a1e2dedec962b43cf10d78652
Matt Diesel <md639@cam.ac.uk>
2016-05-27 01:22:39 +0000

    Added doc headers to sourcepos.

--- com/sourcepos.cc
+++ com/sourcepos.cc
@@ -10,6 +10,10 @@
 
 std::set<std::string> SourcePos::fileNames;
 
+/**
+ *
+ * @author Diesel
+ */
 SourcePos::SourcePos()
     : SourcePos( "", 0, 0, 0 ) {
 }
@@ -30,20 +34,37 @@ SourcePos::SourcePos( std::string file, int line, int col, int abs )
 }
 
 
+/** Return the string file name
+ *
+ * @author Diesel
+ */
 const std::string& SourcePos::fileStr() const {
     return *File;
 }
 
+
+/** Sets the file name.
+ *
+ * @author Diesel
+ */
 void SourcePos::setFile(std::string f) {
     File = getFile(f);
 }
 
 
+/** Returns an iterator the the file name within the set
+ *
+ * @author Diesel
+ */
 std::set<std::string>::const_iterator SourcePos::getFile(std::string f) {
     return fileNames.insert(f).first;
 }
 
 
+/** Streams a source position to an output stream.
+ *
+ * @author Diesel
+ */
 std::ostream& operator<<( std::ostream& os, const SourcePos& sp ) {
     return( os << sp.Line << ":" << sp.Column );
 }
--- com/sourcepos.h
+++ com/sourcepos.h
@@ -7,28 +7,62 @@
 #define GF2_SOURCEPOS_H
 
 
-// Represents a position within a file.
+/** A character position within a source file.
+ *
+ * @author Diesel
+ */
 struct SourcePos {
 private:
     static std::set<std::string> fileNames;
+
+    /** Returns an iterator the the file name within the set
+     *  This allows string sharing between SourcePos instances.
+     *
+     * @param[in]  f    The string file name.
+     *
+     * @return     The iterator to the file name in the table.
+     */
     std::set<std::string>::const_iterator getFile(std::string f);
 
 public:
+    /// The file name
     std::set<std::string>::const_iterator File;
+    /// The line number
 	int Line;
+    /// The character column
 	int Column;
+    /// The absolute character position within the file.
 	int Abs;
 
+    /** Return the string file name
+     *
+     * @return     The string file name
+     */
     const std::string& fileStr() const;
+
+    /** Sets the file name.
+     *
+     * @param[in]  f     The string file name
+     */
     void setFile(std::string f);
 
+    /** Creates and populates a new SourcePos struct instance.
+     */
 	SourcePos();
     SourcePos(int line, int col);
     SourcePos(int line, int col, int abs);
 	SourcePos( std::string file, int line, int col, int abs);
 
-	friend std::ostream& operator<<( std::ostream&, const SourcePos& );
+    /** Streams a source position to an output stream.
+     *
+     * @param      The output stream
+     * @param      The source position
+     *
+     * @return     A reference to the output stream so that streaming operators
+     *             can be chained.
+     */
+	friend std::ostream& operator<<( std::ostream& os, const SourcePos& pos);
 };
 
 
-#endif
+#endif // GF2_SOURCEPOS_H
