c86fa9440335ea56c6fe505a72d86b3f9780c581
Matt March <mdm46@cam.ac.uk>
2016-05-31 10:55:46 +0100

    Removed blank english translation and added check in mattlab.cc instead

--- gui/mattlab.cc
+++ gui/mattlab.cc
@@ -18,8 +18,7 @@ bool MyApp::OnInit()
     // sort out locale for translations before GUI contructed.
     m_locale = new wxLocale(wxLANGUAGE_DEFAULT, wxLOCALE_DONT_LOAD_DEFAULT);
     m_locale->AddCatalogLookupPathPrefix(wxPathOnly(argv[0]) + "/intl");
-    bool translationSuccess = !m_locale->AddCatalog(wxT("mattlab"));
-
+    bool translationFailed = !m_locale->AddCatalog(wxT("mattlab"));
 
     // Construct the GUI
     MyFrame *frame = new MyFrame(NULL, wxDefaultPosition,  wxSize(800, 600));
@@ -29,7 +28,7 @@ bool MyApp::OnInit()
     // Todo: There must be a way of checking if Locale is any English dialect rather than the current
     // loading of a blank translation file for english.
     // Create error message if language catalog was not found.
-    if (translationSuccess) {
+    if (translationFailed && m_locale->GetName().substr(0,2) != "en") {
         wxMessageBox(wxT("No translations available for current language. Reverting to English."),
             wxT("Language not available"), wxOK|wxCENTRE, frame);
     }
